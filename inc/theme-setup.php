fa9261698a46375fad34b03a4eb4824dbf1dd08e
Will <will@pattonwebz.com>
2015-08-18 23:37:30 +0100

    remove section that changes bundeled jQuery version

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -3,38 +3,38 @@
 /* =============================================================
  * Theme Setup Function
  * ============================================================= */
- 
+
 add_action( 'after_setup_theme', 'pw_bestreloaded_setup' );
 
 if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
     function pw_bestreloaded_setup() {
-    
+
         // Set the content width
         if ( ! isset( $content_width ) ) $content_width = 690;
-    
+
         // This theme uses wp_nav_menu() in two locations
         register_nav_menus( array(
             'nav_topbar' => 'Topbar Navigation',
             'nav_footer' => 'Footer Navigation'
         ) );
-        
+
         // Fallback function for Topbar Navigation if it isn't set
         function topbar_nav_fallback() {
             echo '<ul class="nav navbar-nav"><li><a href="' . home_url() . '" title="Home">Home</a></li></ul>';
         }
-        
-        
+
+
         // Fallback function for Footer Navigation if it isn't set
         function footer_nav_fallback() {
             echo '<ul><li><a href="' . home_url() . '" title="Home">Home</a></li></ul>';
         }
-        
+
         // This theme uses Featured Images (also known as post thumbnails)
         add_theme_support( 'post-thumbnails' );
-        
+
         // This feature enables post and comment RSS feed links to head
         add_theme_support( 'automatic-feed-links' );
-    
+
         /* =============================================================
          * Author: Boutros AbiChedid
          * Date: March 20, 2011
@@ -42,14 +42,14 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
          * Description: Numbered Page Navigation (Pagination) Code.
          * Tested: Up to WordPress version 3.1.2 (also works on WP 3.3.1)
          * ============================================================= */
-         
+
         /* Function that Rounds To The Nearest Value.
            Needed for the pagenavi() function */
         function round_num($num, $to_nearest) {
            /*Round fractions down (http://php.net/manual/en/function.floor.php)*/
            return floor($num/$to_nearest)*$to_nearest;
         }
-         
+
         /* Function that performs a Boxed Style Numbered Pagination (also called Page Navigation).
            Function is largely based on Version 2.4 of the WP-PageNavi plugin */
         function pagenavi($before = '', $after = '') {
@@ -68,7 +68,7 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
             $pagenavi_options['always_show'] = 0;
             $pagenavi_options['num_larger_page_numbers'] = 0;
             $pagenavi_options['larger_page_numbers_multiple'] = 3;
-         
+
             //If NOT a single Post is being displayed
             /*http://codex.wordpress.org/Function_Reference/is_single)*/
             if (!is_single()) {
@@ -81,13 +81,13 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                 $paged = intval(get_query_var('paged'));
                 $numposts = $wp_query->found_posts;
                 $max_page = $wp_query->max_num_pages;
-         
+
                 //empty — Determine whether a variable is empty
                 /*http://php.net/manual/en/function.empty.php*/
                 if(empty($paged) || $paged == 0) {
                     $paged = 1;
                 }
-         
+
                 $pages_to_show = intval($pagenavi_options['num_pages']);
                 $larger_page_to_show = intval($pagenavi_options['num_larger_page_numbers']);
                 $larger_page_multiple = intval($pagenavi_options['larger_page_numbers_multiple']);
@@ -96,11 +96,11 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                 //ceil — Round fractions up (http://us2.php.net/manual/en/function.ceil.php)
                 $half_page_end = ceil($pages_to_show_minus_1/2);
                 $start_page = $paged - $half_page_start;
-         
+
                 if($start_page <= 0) {
                     $start_page = 1;
                 }
-         
+
                 $end_page = $paged + $half_page_end;
                 if(($end_page - $start_page) != $pages_to_show_minus_1) {
                     $end_page = $start_page + $pages_to_show_minus_1;
@@ -112,14 +112,14 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                 if($start_page <= 0) {
                     $start_page = 1;
                 }
-         
+
                 $larger_per_page = $larger_page_to_show*$larger_page_multiple;
                 //round_num() custom function - Rounds To The Nearest Value.
                 $larger_start_page_start = (round_num($start_page, 10) + $larger_page_multiple) - $larger_per_page;
                 $larger_start_page_end = round_num($start_page, 10) + $larger_page_multiple;
                 $larger_end_page_start = round_num($end_page, 10) + $larger_page_multiple;
                 $larger_end_page_end = round_num($end_page, 10) + ($larger_per_page);
-         
+
                 if($larger_start_page_end - $larger_page_multiple == $start_page) {
                     $larger_start_page_start = $larger_start_page_start - $larger_page_multiple;
                     $larger_start_page_end = $larger_start_page_end - $larger_page_multiple;
@@ -139,14 +139,14 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                     $pages_text = str_replace("%CURRENT_PAGE%", number_format_i18n($paged), $pagenavi_options['pages_text']);
                     $pages_text = str_replace("%TOTAL_PAGES%", number_format_i18n($max_page), $pages_text);
                     echo $before.'<div class="pagination"><ul>'."\n";
-         
+
                     if(!empty($pages_text)) {
                         echo '<li><span class="pages">'.$pages_text.'</span></li>';
                     }
                     //Displays a link to the previous post which exists in chronological order from the current post.
                     /*http://codex.wordpress.org/Function_Reference/previous_post_link*/
                     echo '<li>'; previous_posts_link($pagenavi_options['prev_text']); echo '</li>';
-         
+
                     if ($start_page >= 2 && $pages_to_show < $max_page) {
                         $first_page_text = str_replace("%TOTAL_PAGES%", number_format_i18n($max_page), $pagenavi_options['first_text']);
                         //esc_url(): Encodes < > & " ' (less than, greater than, ampersand, double quote, single quote).
@@ -157,14 +157,14 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                             echo '<li><span class="expand">'.$pagenavi_options['dotleft_text'].'</span></li>';
                         }
                     }
-         
+
                     if($larger_page_to_show > 0 && $larger_start_page_start > 0 && $larger_start_page_end <= $max_page) {
                         for($i = $larger_start_page_start; $i < $larger_start_page_end; $i+=$larger_page_multiple) {
                             $page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['page_text']);
                             echo '<li><a href="'.esc_url(get_pagenum_link($i)).'" class="single_page" title="'.$page_text.'">'.$page_text.'</a></li>';
                         }
                     }
-         
+
                     for($i = $start_page; $i  <= $end_page; $i++) {
                         if($i == $paged) {
                             $current_page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['current_text']);
@@ -174,7 +174,7 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                             echo '<li><a href="'.esc_url(get_pagenum_link($i)).'" class="single_page" title="'.$page_text.'">'.$page_text.'</a></li>';
                         }
                     }
-         
+
                     if ($end_page < $max_page) {
                         if(!empty($pagenavi_options['dotright_text'])) {
                             echo '<li><span class="expand">'.$pagenavi_options['dotright_text'].'</span></li>';
@@ -183,7 +183,7 @@ if ( !function_exists( 'pw_bestreloaded_setup' ) ) {
                         echo '<li><a href="'.esc_url(get_pagenum_link($max_page)).'" class="last" title="'.$last_page_text.'">'.$max_page.'</a></li>';
                     }
                     echo '<li>'; next_posts_link($pagenavi_options['next_text'], $max_page); echo '</li>';
-         
+
                     if($larger_page_to_show > 0 && $larger_end_page_start < $max_page) {
                         for($i = $larger_end_page_start; $i <= $larger_end_page_end; $i+=$larger_page_multiple) {
                             $page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['page_text']);
@@ -238,8 +238,6 @@ if ( !function_exists( 'load_bestreloaded_scripts' ) ) {
         if ( !is_admin() ) {
             wp_register_script( 'modernizr', get_template_directory_uri() . '/js/libs/modernizr-2.5.3.min.js' );
             wp_enqueue_script( 'modernizr' );
-            wp_deregister_script( 'jquery' );
-            wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', array(), 1.7, true );
             wp_enqueue_script( 'jquery' );
             wp_register_script( 'bestreloaded-plugins', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'bootstrap'), 1.0, true );
             wp_enqueue_script( 'bestreloaded-plugins' );
@@ -260,7 +258,7 @@ add_action( 'wp_head', 'bestreloaded_theme_options' );
 
 if ( !function_exists( 'bestreloaded_theme_options' ) ) {
     function bestreloaded_theme_options() {
-    
+
         $background                = of_get_option( 'bestreloaded_background', 'no entry' );
         $link_color_main           = of_get_option( 'bestreloaded_link_color_main', 'no entry' );
         $link_color_hover_main     = of_get_option( 'bestreloaded_link_hover_color_main', 'no entry' );
@@ -270,9 +268,9 @@ if ( !function_exists( 'bestreloaded_theme_options' ) ) {
         $text_color_featured       = of_get_option( 'bestreloaded_text_color_featured_content' );
         $link_color_featured       = of_get_option( 'bestreloaded_link_color_featured_content' );
         $link_color_hover_featured = of_get_option( 'bestreloaded_link_hover_color_featured_content' ); ?>
-        
+
             <style type="text/css">
-            
+
                 <?php if ( $background ) {
                           if ( $background['color'] && $background['image'] ) { ?>
                               body { background-color: <?php echo $background['color']; ?>;
@@ -289,7 +287,7 @@ if ( !function_exists( 'bestreloaded_theme_options' ) ) {
                               body { background-color: <?php echo $background['color']; ?>; } <?php
                           }
                       } else {
-                          echo 'no entry';    
+                          echo 'no entry';
                       };
                 ?>
                 <?php if ( $background_featured ) {
@@ -308,7 +306,7 @@ if ( !function_exists( 'bestreloaded_theme_options' ) ) {
                               .featured-bar { background-color: <?php echo $background_featured['color']; ?>; } <?php
                           }
                       } else {
-                          echo 'no entry';    
+                          echo 'no entry';
                       };
                 ?>
                 <?php if ( $text_color_featured ) : ?>
@@ -338,9 +336,9 @@ if ( !function_exists( 'bestreloaded_theme_options' ) ) {
                 <?php if ( $link_color_hover_footer ) : ?>
                     footer .container.container-main a:hover { color: <?php echo $link_color_hover_footer; ?>; }
                 <?php endif; ?>
-    
+
             </style>
-    
+
         <?php
     }
     /* ===| end bestreloaded_theme_options() |========================== */
@@ -410,14 +408,14 @@ add_filter( 'widget_tag_cloud_args', 'custom_tag_cloud_widget' );
 if ( !function_exists( 'wp_echo_twitter' ) ) {
     function wp_echo_twitter($username) {
         include_once( ABSPATH . WPINC . '/class-simplepie.php' );
-    
+
         // Fetch feed, set cache locaiton, and initialize function
         $feed = new SimplePie();
         $feed->set_feed_url("http://search.twitter.com/search.atom?q=from:$username");
         $feed->set_cache_location( ABSPATH . WPINC );
         $feed->init();
         $feed->handle_content_type();
-        
+
         // Output tweet
         foreach ($feed->get_items(0, 1) as $item):
             echo '<p class="hero-p" style="margin-bottom: 9px;">"' . $item->get_description() . '"</p>' . '<span><a href="' . $item->get_permalink() . '">' . $item->get_date('D, M j, Y') . '</a></span>';
