71819a19235c1ef9d9549c622104851ce61fdac4
Will <will@pattonwebz.com>
2015-08-23 01:27:21 +0100

    add some debug code to social-float code

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
  * @subpackage Best_Reloaded
  * @since Best Reloaded 0.1
  */
+var debug = false;
 jQuery(document).ready(function($){
     // if #social-block exists then do this...
     // TODO: Add on/off toggle here
@@ -15,9 +16,18 @@ jQuery(document).ready(function($){
         $('#social-block').width($("#main_content").width());
         // get the postion of #social-block inside the viewport
         var socialTop = $('#social-block')[0].getBoundingClientRect().top;
+        if(debug){
+            console.log(socialtop,' initial value of socialTop' );
+        }
         // affixes the div whenever scroll past it
         if ( $('header nav.navbar').hasClass('navbar-fixed-top') ){
             socialTop = socialTop + 50;
+            if(debug){
+                console.log(socialtop,' updated value of socialTop' );
+            }
+        }
+        if(debug){
+            console.log(socialtop,' using this value for socialTop' );
         }
         $('#social-block').affix({
             offset: { top: socialTop }
