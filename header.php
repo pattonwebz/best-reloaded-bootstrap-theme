c735ef7f6641ff83c0221394ea7db8123afa8016
William Patton <will@pattonwebz.com>
2017-01-24 19:50:42 +0000

    Removed the brand from navbar - not used here

--- header.php
+++ header.php
@@ -32,10 +32,10 @@
     <header>
 
 		<nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+			
 		  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    	<span class="navbar-toggler-icon"></span>
 		  	</button>
-		  	<a class="navbar-brand" href="#">Navbar</a>
 
 		  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<?php
