bbf0edcdce167438f372714847c2bfecd1e041be
William Patton <will@pattonwebz.com>
2017-02-12 05:20:32 +0000

    added some functions and styles for gravity forms

--- functions.php
+++ functions.php
@@ -150,3 +150,12 @@ function add_class_to_items_link( $atts, $item, $args ) {
 function of_get_option($key = false, $default = false){
 	return get_option( $key );
 }
+
+//Filter to add some bootstrap specific classes to gravity forms
+//
+add_filter( 'gform_field_container', 'add_bootstrap_container_class', 10, 6 );
+function add_bootstrap_container_class( $field_container, $field, $form, $css_class, $style, $field_content ) {
+  $id = $field->id;
+  $field_id = is_admin() || empty( $form ) ? "field_{$id}" : 'field_' . $form['id'] . "_$id";
+  return '<li id="' . $field_id . '" class="' . $css_class . ' form-group">{FIELD_CONTENT}</li>';
+}
