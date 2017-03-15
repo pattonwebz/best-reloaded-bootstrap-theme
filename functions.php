bc4881d133ef245686530f2d1492f87d7461eaeb
William Patton <will@pattonwebz.com>
2017-03-15 06:52:09 +0000

    Use `get_template_directory()` when requiring php files, move navwalker file to inside /inc/ directry

--- functions.php
+++ functions.php
@@ -12,26 +12,26 @@
 if ( ! isset( $content_width ) ) $content_width = 730;
 
 // Contains all functions essential to setting the theme
-require 'inc/theme-setup.php';
+require( trailingslashit( get_template_directory() ) . 'inc/theme-setup.php' );
 // Sets up comments and pingbacks for the theme
-require 'inc/comments-and-pingbacks.php';
+require( trailingslashit( get_template_directory() ) . 'inc/comments-and-pingbacks.php' );
 // Registers all dynamic sidebar areas for the theme
-require 'inc/register-sidebars.php';
+require( trailingslashit( get_template_directory() ) . 'inc/register-sidebars.php' );
 // Register Custom Navigation Walker
-require_once('wp_bootstrap_navwalker.php');
-
-require 'inc/customizer.php';
+require( trailingslashit( get_template_directory() ) . 'inc/wp_bootstrap_navwalker.php' );
+// Adds customizer controls, settings and sanitization functions
+require( trailingslashit( get_template_directory() ) . 'inc/customizer.php' );
 
 // Move the Jetpack social share buttons to the beginning of the post
 function pwwp_jptweak_remove_share() {
-remove_filter( 'the_content', 'sharing_display',19 );
-remove_filter( 'the_excerpt', 'sharing_display',19 );
-remove_filter( 'the_content', array( 'Jetpack_Likes', 'post_likes' ), 30, 1 );
+	remove_filter( 'the_content', 'sharing_display',19 );
+	remove_filter( 'the_excerpt', 'sharing_display',19 );
+	remove_filter( 'the_content', array( 'Jetpack_Likes', 'post_likes' ), 30, 1 );
 }
 
 add_action( 'loop_end', 'pwwp_jptweak_remove_share' );
 
-//Add classnames to the tags for bootstrap markup
+// Add classnames to the tags for bootstrap markup
 function pwwp_add_class_the_tags($html){
     $postid = get_the_ID();
     $html = str_replace('<a','<a class="label label-default"',$html);
@@ -50,10 +50,9 @@ function pwwp_remove_recent_comments_style() {
 add_action( 'widgets_init', 'pwwp_remove_recent_comments_style' );
 
 //Filter to add some bootstrap specific classes to gravity forms if installed
-add_filter( 'gform_field_container', 'add_bootstrap_container_class', 10, 6 );
-
 function add_bootstrap_container_class( $field_container, $field, $form, $css_class, $style, $field_content ) {
-  $id = $field->id;
-  $field_id = is_admin() || empty( $form ) ? "field_{$id}" : 'field_' . $form['id'] . "_$id";
-  return '<li id="' . $field_id . '" class="' . $css_class . ' form-group">{FIELD_CONTENT}</li>';
+	$id = $field->id;
+	$field_id = is_admin() || empty( $form ) ? "field_{$id}" : 'field_' . $form['id'] . "_$id";
+	return '<li id="' . $field_id . '" class="' . $css_class . ' form-group">{FIELD_CONTENT}</li>';
 }
+add_filter( 'gform_field_container', 'add_bootstrap_container_class', 10, 6 );
