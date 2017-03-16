bbb819f90c2566cc3516451f37e149f49cbcdb1a
William Patton <will@pattonwebz.com>
2017-03-16 02:17:40 +0000

    Remove the old IE7 or earlier upgarde message from header

--- header.php
+++ header.php
@@ -18,7 +18,6 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
     <header>
 
 		<nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
