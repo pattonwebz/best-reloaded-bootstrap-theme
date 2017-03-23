e3fb98e0e64a0ca82d78179ac22f4438b5e5a38a
William Patton <will@pattonwebz.com>
2017-03-23 13:50:24 +0000

    use esc_attr_e for aria-label instead of esc_html_e

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 			<div class="container">
 
-			  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle navigation', 'best-reloaded' ); ?>">
+			  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'best-reloaded' ); ?>">
 			    	<span class="navbar-toggler-icon"></span>
 			  	</button>
 
