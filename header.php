8719be2aacc3f2a1a3ec9ce6c9c6f480ffa8cd30
William Patton <will@pattonwebz.com>
2017-03-23 10:09:59 +0000

    Translate aria-label

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 			<div class="container">
 
-			  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle navigation', 'best-reloaded' ); ?>">
 			    	<span class="navbar-toggler-icon"></span>
 			  	</button>
 
