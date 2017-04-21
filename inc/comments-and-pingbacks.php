812c9126f663caa512e32169cefd04f9866ebb32
William Patton <will@pattonwebz.com>
2017-04-21 20:44:56 +0100

    fix unreachable code message

--- inc/comments-and-pingbacks.php
+++ inc/comments-and-pingbacks.php
@@ -16,58 +16,60 @@
  * @param  integer $depth		a current depth value.
  */
 function best_reloaded_respond_comment( $comment, $args, $depth ) {
-	switch ( $comment->comment_type ) :
+	switch ( $comment->comment_type ) {
 		case 'pingback' :
+			// for pingbacks theres no reponses.
 		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php esc_html_e( 'Pingback:', 'best-reloaded' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( esc_html__( 'Edit.', 'best-reloaded' ) . ' &#x270E;', ' </br><span class="edit-link">', '</span>' ); ?></p>
-	<?php
+			// trackbacks can be responded to/edited.?>
+			<li class="post pingback">
+				<p><?php esc_html_e( 'Pingback:', 'best-reloaded' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( esc_html__( 'Edit.', 'best-reloaded' ) . ' &#x270E;', ' </br><span class="edit-link">', '</span>' ); ?></p>
+			<?php
 			break;
 		default :
-	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<article id="comment-<?php comment_ID(); ?>" class="comment clearfix">
-			<footer class="comment-meta">
-				<div class="comment-author vcard clearfix">
-					<?php
-						$avatar_size = 48;
+			// in all other default cases we can assume normal comment.	?>
+			<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+				<article id="comment-<?php comment_ID(); ?>" class="comment clearfix">
+					<footer class="comment-meta">
+						<div class="comment-author vcard clearfix">
+							<?php
+								$avatar_size = 48;
 
-						echo get_avatar( $comment, $avatar_size );
+								echo get_avatar( $comment, $avatar_size );
 
-						/* translators: 1: comment author, 2: date and time */
-						printf( '%1$s %2$s',
-							sprintf( '<span class="fn">%s</span><br/>', get_comment_author_link() ),
-							sprintf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
-								esc_url( get_comment_link( $comment->comment_ID ) ),
-								get_comment_time( 'c' ),
-								/* translators: 1: date, 2: time */
-								sprintf( '%1$s at %2$s', get_comment_date(), get_comment_time() )
-							)
-						);
-					?>
-				</div><!-- .comment-author .vcard -->
+								/* translators: 1: comment author, 2: date and time */
+								printf( '%1$s %2$s',
+									sprintf( '<span class="fn">%s</span><br/>', get_comment_author_link() ),
+									sprintf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+										esc_url( get_comment_link( $comment->comment_ID ) ),
+										get_comment_time( 'c' ),
+										/* translators: 1: date, 2: time */
+										sprintf( '%1$s at %2$s', get_comment_date(), get_comment_time() )
+									)
+								);
+							?>
+						</div><!-- .comment-author .vcard -->
 
-				<?php if ( '0' === $comment->comment_approved ) : ?>
-					<em class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.', 'best-reloaded' ); ?></em>
-					<br />
-				<?php endif; ?>
+						<?php if ( '0' === $comment->comment_approved ) : ?>
+							<em class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.', 'best-reloaded' ); ?></em>
+							<br />
+						<?php endif; ?>
 
-			</footer>
+					</footer>
 
-			<div class="comment-content"><?php comment_text(); ?></div>
+					<div class="comment-content"><?php comment_text(); ?></div>
 
-			<div class="edit-reply">
-				<?php edit_comment_link( esc_html__( 'Edit.', 'best-reloaded' ) . ' &#x270E;', '<span class="edit-link">', '</span>' ); ?>
-				<?php comment_reply_link( array_merge( $args, array(
-					'reply_text' => esc_html__( 'Reply ', 'best-reloaded' ) . ' <span>&#x21A9;</span>',
-					'depth' => $depth,
-					'max_depth' => $args['max_depth'],
-				) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-## -->
+					<div class="edit-reply">
+						<?php edit_comment_link( esc_html__( 'Edit.', 'best-reloaded' ) . ' &#x270E;', '<span class="edit-link">', '</span>' ); ?>
+						<?php comment_reply_link( array_merge( $args, array(
+							'reply_text' => esc_html__( 'Reply ', 'best-reloaded' ) . ' <span>&#x21A9;</span>',
+							'depth' => $depth,
+							'max_depth' => $args['max_depth'],
+						) ) ); ?>
+					</div><!-- .reply -->
+				</article><!-- #comment-## -->
 
-	<?php
-			break;
-	endswitch;
+				<?php
+				break;
+		// no more cases to test for.
+	} // End switch().
 }
