0c37106221e942e168ce0300ba152e631c0a1ab8
William Patton <will@pattonwebz.com>
2017-07-05 14:28:36 +0100

    swap to using a hook/filter system for featured text

--- page.php
+++ page.php
@@ -10,7 +10,8 @@
 
 ?>
 <?php get_header(); ?>
-		<?php get_template_part( 'inc/parts/featured', 'bar' ); ?>
+		<?php // This is the hook used to add featurebar content.
+		best_reloaded_do_featurebar(); ?>
 
 		<div class="row">
 			<div class="col-md-8">
