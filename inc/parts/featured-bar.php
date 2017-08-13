232134db224a3be93e8df7185b6abb565b776325
William Patton <will@pattonwebz.com>
2017-08-13 01:52:34 +0100

    Whitespace fix

--- inc/parts/featured-bar.php
+++ inc/parts/featured-bar.php
@@ -13,7 +13,7 @@
 <?php if ( get_theme_mod( 'display_featured_bar' ) && get_theme_mod( 'featured_bar' ) ) { ?>
 	<section class="featured-bar text-center">
 		<p class="lead">
-				<?php echo do_shortcode( get_theme_mod( 'featured_bar' ) ); ?>
+			<?php echo do_shortcode( get_theme_mod( 'featured_bar' ) ); ?>
 		</p>
 	</section>
 <?php } ?>
