8bf8eb17c037363d5b790448fc64fac413ec7be8
William Patton <will@pattonwebz.com>
2017-10-19 18:05:33 +0100

    Remove unnecessary class existence check   The file checks class existance before adding on it's own.

--- functions.php
+++ functions.php
@@ -16,10 +16,7 @@ require( trailingslashit( get_template_directory() ) . 'inc/comments-and-pingbac
 // Registers all dynamic sidebar areas for the theme.
 require( trailingslashit( get_template_directory() ) . 'inc/register-sidebars.php' );
 // Register Custom Navigation Walker.
-// This is a 3rd party class - only require if the class isn't added already.
-if ( ! class_exists( 'wp_bootstrap_navwalker', false ) ) {
-	require( trailingslashit( get_template_directory() ) . 'inc/class-wp-bootstrap-navwalker.php' );
-}
+require( trailingslashit( get_template_directory() ) . 'inc/class-wp-bootstrap-navwalker.php' );
 // Adds customizer controls, settings and sanitization functions.
 require( trailingslashit( get_template_directory() ) . 'inc/customizer.php' );
 // Custom functions.
