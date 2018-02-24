844696f4886ff99553adc144bbc9ba8d4974398f
William Patton <will@pattonwebz.com>
2018-02-24 01:00:22 +0000

    Add option to select num posts output in frontpage loop

--- inc/parts/loop-home.php
+++ inc/parts/loop-home.php
@@ -1,12 +1,12 @@
 <?php
- /**
-  * The loop-home.php file.
-  *
-  * Outputs the main homepage posts loop
-  *
-  * @package Best_Reloaded
-  * @since Best Reloaded v0.1
-  */
+/**
+ * The loop-home.php file.
+ *
+ * Outputs the main homepage posts loop
+ *
+ * @package Best_Reloaded
+ * @since Best Reloaded v0.1
+ */
 
 ?>
 <div class="row blog-three-up">
@@ -16,7 +16,8 @@
 			dynamic_sidebar( 'frontpage-blog-row-sidebar' );
 		} else {
 			the_widget( 'WP_Widget_Tag_Cloud' );
-		} ?>
+		}
+		?>
 	</div><!-- end .col-sm-3 -->
 	<?php
 	// get the global that stores posts we don't want duplicated here.
@@ -28,31 +29,42 @@
 	// sticky posts mess with the loop counter, merge them into the array we
 	// don't want to duplciate.
 	$best_excluded_ids = array_merge( $best_do_not_get_duplicates, get_option( 'sticky_posts' ) );
+
 	$args = array(
-		'posts_per_page' 	=> 3,
-		'post__not_in' 		=> $best_excluded_ids,
+		'posts_per_page' => get_theme_mod( 'homepage_posts_output_num', 3 ),
+		'post__not_in'   => $best_excluded_ids,
 	);
 	$loop = new WP_Query( $args );
-	if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();	?>
-	<div class="col-sm-12 col-md">
-		<article  <?php post_class(); ?> >
-			<header>
-				<a href="<?php the_permalink(); ?>" class="post-thumb">
-					<div class="hoverspan">
-						<?php get_template_part( 'inc/parts/featured', 'image' ); ?>
-					</div>
-				</a>
-				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			</header>
-			<?php the_excerpt(); ?>
-			<?php best_reloaded_do_post_meta(); ?>
-		</article>
-		<hr class="hr-row-divider">
-	</div><!-- end col-sm-3 -->
-	<?php endwhile; else : ?>
+
+	if ( $loop->have_posts() ) {
+		while ( $loop->have_posts() ) {
+			$loop->the_post();
+			?>
+			<div class="col-sm-12 col-md">
+				<article  <?php post_class(); ?> >
+					<header>
+						<a href="<?php the_permalink(); ?>" class="post-thumb">
+							<div class="hoverspan">
+								<?php get_template_part( 'inc/parts/featured', 'image' ); ?>
+							</div>
+						</a>
+						<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					</header>
+					<?php the_excerpt(); ?>
+					<?php best_reloaded_do_post_meta(); ?>
+				</article>
+				<hr class="hr-row-divider">
+			</div><!-- end col-sm-3 -->
+			<?php
+		}
+	} else {
+	?>
 	<div class="col-sm-9">
 		<p class="hero-p no-content-message"><?php esc_html_e( 'There is currently nothing to display.', 'best-reloaded' ); ?></p>
 	</div>
-	<?php endif; ?>
+	<?php
+	}
+	?>
 </div><!-- end .row -->
-<?php wp_reset_postdata();
+<?php
+wp_reset_postdata();
