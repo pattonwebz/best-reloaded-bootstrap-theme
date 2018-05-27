e7f16b7ae1f9c237cce276e1652745f878e97e77
William Patton <will@pattonwebz.com>
2018-05-27 13:55:51 +0100

    add field and styles for cookies-consent in comments form

--- comments.php
+++ comments.php
@@ -86,6 +86,7 @@
 		$commenter = wp_get_current_commenter();
 		$req       = get_option( 'require_name_email' );
 		$aria_req  = ( $req ? " aria-required='true'" : '' );
+		$consent   = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
 		comment_form(
 			array(
 				'comment_field'       =>
@@ -96,20 +97,22 @@
 				'label_submit'        => esc_html__( 'Leave Comment', 'best-reloaded' ),
 				'comment_notes_after' => '',
 				'fields'              => array(
-					'author' =>
+					'author'  =>
 						'<div class="comment-form-author form-group row"><label for="author" class="col-2">' . __( 'Name', 'best-reloaded' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
 						'<input id="author" name="author" type="text" class="form-control col-10" value="' . esc_attr( $commenter['comment_author'] ) .
 						'" size="30"' . $aria_req . ' /></div>',
 
-					'email'  =>
+					'email'   =>
 						'<div class="comment-form-email form-group row"><label for="email" class="col-2">' . __( 'Email', 'best-reloaded' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
 						'<input id="email" name="email" type="email" class="form-control col-10" value="' . esc_attr( $commenter['comment_author_email'] ) .
 						'" size="30"' . $aria_req . ' /></div>',
 
-					'url'    =>
+					'url'     =>
 						'<div class="comment-form-url form-group row"><label for="url" class="col-2">' . __( 'Website', 'best-reloaded' ) . '</label>' .
 						'<input id="url" name="url" type="text" class="form-control col-10" value="' . esc_attr( $commenter['comment_author_url'] ) .
 						'" size="30" /></div>',
+					'cookies' => '<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' />' .
+						'<label for="wp-comment-cookies-consent">' . esc_html__( 'Save my name, email, and website in this browser for the next time I comment.', 'best-reloaded' ) . '</label></p>',
 				),
 				'class_submit'        => 'submit btn btn-lg btn-theme',
 			)
